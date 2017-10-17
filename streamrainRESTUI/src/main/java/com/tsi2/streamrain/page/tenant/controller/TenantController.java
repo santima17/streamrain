a5b17fa40959f9d37c479090fa552c9901935478
Santiago <santiago.marquez@pyxis.com.uy>
2017-10-16 23:44:26 -0300

    page for register tenant

--- streamrainRESTUI/src/main/java/com/tsi2/streamrain/page/tenant/controller/TenantController.java
+++ streamrainRESTUI/src/main/java/com/tsi2/streamrain/page/tenant/controller/TenantController.java
@@ -1,8 +1,10 @@
-package com.tsi2.streamrain.page.main.controller;
+package com.tsi2.streamrain.page.tenant.controller;
 
 import javax.annotation.Resource;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -18,27 +20,14 @@ public class TenantController {
 	private static final String USER_PREFIX = "/tenant/";
 	
 	@RequestMapping(value = "/registerTenant", method = RequestMethod.GET)
-	public String showRegisterTenant () {	
+	public String showRegisterTenant (Model model) {	
 		TenantDto tenant = new TenantDto();
-		tenant.setId(3);
-		tenant.setIp("localhost");
-		tenant.setName("generator3");
-		tenant.setPassword("apagon23");
-		tenant.setPort(3306);
-		tenant.setUser("root");
-		tenantService.saveTenant(tenant);
+		model.addAttribute("tenantForm", tenant);
 		return USER_PREFIX + "registerTenant";
 	}
 	
 	@RequestMapping(value = "/registerTenant", method = RequestMethod.POST)
-	public String saveTenant () {
-		TenantDto tenant = new TenantDto();
-		tenant.setId(3);
-		tenant.setIp("localhost");
-		tenant.setName("generator3");
-		tenant.setPassword("apagon23");
-		tenant.setPort(3306);
-		tenant.setUser("root");
+	public String saveTenant (@ModelAttribute("tenantForm") TenantDto tenant) {
 		tenantService.saveTenant(tenant);
 		return USER_PREFIX + "registerTenant";
 	}
